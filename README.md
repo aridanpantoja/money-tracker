f6faf27ef855fdbcb5d14bd284f7502be7b43bb0
Aridan <aridanpantoja@gmail.com>
2024-09-06 15:19:00 -0300

    refactor: organiza estrutura, melhora o codigo e ajusta HTML

---
+++ README.md
@@ -0,0 +1,7 @@
+# Money Tracker - Santander Coders 2024
+
+Projeto feito para o módulo de Lógica de Programação
+
+## Tech Stack
+
+[![My Skills](https://skillicons.dev/icons?i=js,html,css)](https://skillicons.dev)
---
+++ js/script.js
@@ -0,0 +1,98 @@
+const gastos = [
+    { mes: 'janeiro', categorias: { energia: 0, agua: 0, aluguel: 0, escola: 0, supermercado: 0 } },
+    { mes: 'fevereiro', categorias: { energia: 0, agua: 0, aluguel: 0, escola: 0, supermercado: 0 } },
+    { mes: 'marco', categorias: { energia: 0, agua: 0, aluguel: 0, escola: 0, supermercado: 0 } },
+    { mes: 'abril', categorias: { energia: 0, agua: 0, aluguel: 0, escola: 0, supermercado: 0 } },
+    { mes: 'maio', categorias: { energia: 0, agua: 0, aluguel: 0, escola: 0, supermercado: 0 } },
+    { mes: 'junho', categorias: { energia: 0, agua: 0, aluguel: 0, escola: 0, supermercado: 0 } },
+    { mes: 'julho', categorias: { energia: 0, agua: 0, aluguel: 0, escola: 0, supermercado: 0 } },
+    { mes: 'agosto', categorias: { energia: 0, agua: 0, aluguel: 0, escola: 0, supermercado: 0 } },
+    { mes: 'setembro', categorias: { energia: 0, agua: 0, aluguel: 0, escola: 0, supermercado: 0 } },
+    { mes: 'outubro', categorias: { energia: 0, agua: 0, aluguel: 0, escola: 0, supermercado: 0 } },
+    { mes: 'novembro', categorias: { energia: 0, agua: 0, aluguel: 0, escola: 0, supermercado: 0 } },
+    { mes: 'dezembro', categorias: { energia: 0, agua: 0, aluguel: 0, escola: 0, supermercado: 0 } },
+];
+
+const mesDropdown = document.querySelector('#mes-dropdown');
+const tituloMes = document.querySelector('#titulo-mes');
+const estatisticasGastos = document.querySelector('#estatisticas-gastos');
+const formularioGastos = document.querySelector('#formulario-gastos');
+
+const orcamento = 8500;
+
+function getRemoveId(categoria, mes) {
+    return `remove-${categoria}-${mes}`
+}
+
+function getUpdateId(categoria, mes) {
+    return `update-${categoria}-${mes}`
+}
+
+function criaCardGasto(mesEncontrado, categoriaGasto, valor) {
+    const categoriaLabel = document.querySelector(`option[value="${categoriaGasto}"]`).innerText;
+    const mes = mesEncontrado.mes;
+    const removeId = getRemoveId(categoriaGasto, mes);
+    const updateId = getUpdateId(categoriaGasto, mes);
+
+    const cardGasto = document.createElement("div");
+    cardGasto.innerHTML = `
+        <h3>${categoriaLabel}</h3>
+        <p>R$<span id="${updateId}">${valor.toFixed(2)}</span></p>
+        <button id="${removeId}">X</button>
+    `;
+
+    estatisticasGastos.appendChild(cardGasto);
+
+    document.getElementById(removeId).addEventListener('click', () => {
+        removeGasto(mesEncontrado, categoriaGasto, cardGasto);
+        console.log(gastos);
+    });
+}
+
+function removeGasto(mesEncontrado, categoriaGasto, cardGasto) {
+    mesEncontrado.categorias[categoriaGasto] = 0;
+    estatisticasGastos.removeChild(cardGasto);
+}
+
+function atualizaGasto(mesEncontrado, categoriaGasto, valor) {
+    alert("Você está atualizando um gasto");
+    const updateId = getUpdateId(categoriaGasto, mesEncontrado.mes);
+    const valorGasto = document.getElementById(updateId);
+    console.log(valorGasto);
+
+    mesEncontrado.categorias[categoriaGasto] = valor;
+    valorGasto.innerText = valor.toFixed(2);
+}
+
+formularioGastos.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let inputGasto = document.querySelector('#valor-gasto').value;
+    const valorGasto = parseFloat(inputGasto);
+    const categoriaGasto = document.querySelector('#categoria-gasto').value;
+
+    // Adiciona despesa
+    const mesEncontrado = gastos.find(item => item.mes === mesDropdown.value);
+
+    if (mesEncontrado.categorias[categoriaGasto] === 0) {
+        mesEncontrado.categorias[categoriaGasto] = valorGasto;
+        criaCardGasto(mesEncontrado, categoriaGasto, valorGasto);
+    } else {
+        mesEncontrado.categorias[categoriaGasto] = valorGasto;
+        atualizaGasto(mesEncontrado, categoriaGasto, valorGasto);
+    }
+
+    inputGasto = "";
+})
+
+
+mesDropdown.addEventListener('change', (e) => {
+    const mesEncontrado = gastos.find(item => item.mes === mesDropdown.value);
+    tituloMes.innerText = mesDropdown.options[mesDropdown.selectedIndex].text;
+    estatisticasGastos.innerHTML = "";
+
+    for (let categoria in mesEncontrado.categorias) {
+        if (mesEncontrado.categorias[categoria] !== 0) {
+            criaCardGasto(mesEncontrado, categoria, mesEncontrado.categorias[categoria]);
+        }
+    }
+});
